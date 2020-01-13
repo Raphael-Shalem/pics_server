53be9e67c617cefd0eb6bd5288a38fc248b2945f
Raphael Shalem <raphael.shalem@gmail.com>
2020-01-13 17:14:48 +0200

    sdfgh

--- src/index.js
+++ src/index.js
@@ -21,7 +21,8 @@ app.use(cors(cors_options));
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", process.env.ORIGIN || "*");
+//  res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 
